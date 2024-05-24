ed5e99a4d839eb32fb373bdcea0f69ab8d075b7e
omondi-mutuma <omondimutuma@gmail.com>
2024-05-24 12:39:35 +0300

    First ada commit

---
+++ create_files.sh
@@ -0,0 +1,24 @@
+/loveLace/
+├── /ada-css/
+│   ├── ada-base.css
+│   ├── ada-layout.css
+│   └── ada-components.css
+├── /ada-js/
+│   ├── ada-main.js
+│   └── ada-utils.js
+├── /ada-images/
+│   ├── ada-logo.png
+│   └── ada-header-banner.jpg
+├── /ada-about/
+│   └── index.html
+├── /ada-services/
+│   ├── ada-web-development.html
+│   ├── ada-seo-services.html
+│   └── ada-content-writing.html
+├── /ada-blog/
+│   ├── index.html
+│   ├── ada-my-first-blog-post.html
+│   └── ada-seo-tips.html 
+├── /ada-contact/
+│   └── index.html
+└── index.html
