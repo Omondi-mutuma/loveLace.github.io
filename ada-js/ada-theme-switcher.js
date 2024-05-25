5e0d34f496f87e34ee61d95a5a62b015e214ddf9
omondi-mutuma <omondimutuma@gmail.com>
2024-05-25 12:41:17 +0300

    Updates to Master

--- ada-js/ada-theme-switcher.js
+++ ada-js/ada-theme-switcher.js
@@ -1,14 +1,2 @@
-function switchTheme(theme) {
-    document.documentElement.setAttribute('data-theme', theme);
-  }
-  
-  // Example usage: Switch to dark theme
-  document.getElementById('theme-toggle').addEventListener('click', () => {
-    const currentTheme = document.documentElement.getAttribute('data-theme');
-    if (currentTheme === 'dark') {
-      switchTheme('default');
-    } else {
-      switchTheme('dark');
-    }
-  });
-  
+
+ 
