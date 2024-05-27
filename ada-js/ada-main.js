474547afcb87e8f0972af7710d2e6899e7884501
omondi-mutuma <omondimutuma@gmail.com>
2024-05-27 18:25:26 +0300

    updates to preloader

--- ada-js/ada-main.js
+++ ada-js/ada-main.js
@@ -1,11 +1,8 @@
 $(document).ready(function() {
-  $('has-animation').each(function() {
+  $('.has-animation').each(function() {
     var $this = $(this);
     var delay = $this.data('delay') || 0;
-    console.log('Element:', $this); // Log element to console
-    console.log('Delay:', delay);   // Log delay to console
     setTimeout(function() {
-      console.log('Adding animate-in to:', $this); // Log before adding class
       $this.addClass('animate-in');
     }, delay);
   });
